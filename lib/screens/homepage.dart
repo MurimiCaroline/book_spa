7b8cf9883ded0713207ec33420f6551112969d92
MurimiCaroline <murimicaroline20@gmail.com>
2023-08-16 08:56:25 +0300

    day two. added the hover feature and gesture detector on the images.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -26,22 +26,92 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ],
           ),
-          const Row(
-            children: [
-              Image(
-                image: AssetImage('images/luxuryspa.jpg'),
-                height: 200,
-                width: 150,
+          const SizedBox(height: 10),
+           Row(
+            children: [              
+              GestureDetector(
+                onTap: (){
+                    // ignore: avoid_print
+                    print("cow");
+                },
+                 
+                child: const _ImageWithHoverHighlight(
+                 imagePath: 'images/luxuryspa.jpg',
+                  height: 200,
+                  width: 150,
+                ),
               ),
-              Image(
-                image: AssetImage('images/glamourspa.jpg'),
-                height: 200,
-                width: 150,
+               GestureDetector(
+                onTap: (){
+                    // ignore: avoid_print
+                    print("cat");
+                },
+                 
+                child: const _ImageWithHoverHighlight(
+                 imagePath: 'images/glamourspa.jpg',
+                  height: 200,
+                 width: 150,
+                                ),
               ),
-            ],
+              ],
+      )
+            ],      
+            ),
+    
+      );
+  }
+}
+
+class _ImageWithHoverHighlight extends StatefulWidget {
+  final String imagePath;
+  final double width;
+  final double height;
+
+  const _ImageWithHoverHighlight({
+    required this.imagePath,
+    required this.width,
+    required this.height,
+  });
+
+  @override
+  __ImageWithHoverHighlightState createState() =>
+      __ImageWithHoverHighlightState();
+}
+
+class __ImageWithHoverHighlightState extends State<_ImageWithHoverHighlight> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() {
+          isHovered = true;
+        });
+      },
+      onExit: (_) {
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: Stack(
+        children: [
+          Image.asset(
+            widget.imagePath,
+            width: widget.width,
+            height: widget.height,
           ),
+          if (isHovered)
+            Container(
+              width: widget.width,
+              height: widget.height,
+              color: Colors.grey.withOpacity(0.3),
+            ),
         ],
       ),
     );
   }
 }
+
+
+

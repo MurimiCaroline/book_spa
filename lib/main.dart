f9d0e3ca74fbd5b6423ba0fce2e033439a523fb7
MurimiCaroline <murimicaroline20@gmail.com>
2023-08-25 09:08:44 +0300

    added the log in page and its features

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:book_spa/screens/homepage.dart';
+//  import 'package:book_spa/screens/homepage.dart';
+import 'package:book_spa/screens/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SPA BOOK',
-      home: HomeScreen(),
+      // home: HomeScreen(),   
+      home: LogIn(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
- class LogIn extends StatefulWidget {
+class LogIn extends StatefulWidget {
   const LogIn({super.key});
 
   @override
@@ -10,20 +10,60 @@ import 'package:flutter/material.dart';
 class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-       padding: EdgeInsets.all(16.0),
-           child: Form(
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: "Email"),
-              )
-            ],
+    double containerWidth = MediaQuery.of(context).size.width * 0.4;
+    double containerHeight = MediaQuery.of(context).size.height * 0.4;
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'images/loginbg.jpg',
+            fit: BoxFit.cover,
           ),
+          Center(
+            child: Container(
+              height: containerHeight,
+              width: containerWidth,
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                   const Text(
+                    'LOG IN', // Title
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Email'),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    decoration: InputDecoration(labelText: 'Password'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Handle form submission
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                    ),
+                    child: const Text('Log In'),
+                    
+                  ),
+                ],
+              ),
+            ),
           ),
-        
-         ),
-      );
-      }
+        ],
+      ),
+    );
+  }
 }
